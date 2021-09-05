9359fb5966e234d1523fecde4355e3d6fb43e12d
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-04 22:40:09 -0300

    Adjusted the exercise8

--- src/test/java/exercise9/ValidationByAgeParameters.java
+++ src/test/java/exercise9/ValidationByAgeParameters.java
@@ -1,4 +1,4 @@
-package exercise8;
+package exercise9;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,24 +8,21 @@ import java.util.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 import vote.VoteAge;
 
 @RunWith(Parameterized.class)
-public class ValidationByAge_DDT {
+public class ValidationByAgeParameters {
 
 	// Attributes
+	@Parameter(0)
 	public String name;
+	@Parameter(1)
 	public int birthday;
+	@Parameter(2)
 	public String result;
-
-	// Constructor
-	public ValidationByAge_DDT(String name, int birthday, String result) {
-		this.name = name;
-		this.birthday = birthday;
-		this.result = result;
-	}
 
 	// Test method
 	@Test
@@ -35,10 +32,11 @@ public class ValidationByAge_DDT {
 
 	@Parameters(name = " {0} | {1} | {2} ")
 	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] { { "Angela", 2007, "Angela you can't vote" },
-				{ "Maria", 2006, "Maria you can't vote" }, { "Nikolai", 2005, "Nikolai your vote is optional" },
-				{ "Jhon", 2004, "Jhon your vote is optional" }, { "Wendy", 2003, "Wendy your vote is mandatory" },
-				{ "Paul", 1996, "Paul your vote is mandatory" }, { "Robert", 1951, "Robert your vote is mandatory" },
+		return Arrays.asList(new Object[][] { { "Maria", 2006, "Maria you can't vote" },
+				{ "Nikolai", 2005, "Nikolai your vote is optional" }, { "Jhon", 2004, "Jhon your vote is optional" },
+				{ "Wendy", 2003, "Wendy your vote is mandatory" }, { "Paul", 1996, "Paul your vote is mandatory" },
+				{ "Robert", 1951, "Robert your vote is mandatory" },
 				{ "Jhony", 1950, "Jhony your vote is optional" } });
 	}
+
 }
